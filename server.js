723cbbbc9b0290102520d62a9c18d8862dcd6839
Arian N. Yamchelo <ariannajafi@icloud.com>
2023-09-04 19:37:20 +1000

    Connect to database

--- server.js
+++ server.js
@@ -8,26 +8,31 @@ const cors = require("cors");
 const port = process.env.PORT || 3000;
 
 // Database connection ---------------------------------------------
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("db connected!");
+    }, err => {
+        console.log("db connection failed!", err);
+    });
 
 // Express app setup -----------------------------------------------
-const app =  express();
+const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use("*", cors());
 
 // Setup routes ----------------------------------------------------
-
 // Homepage route
 app.get('/', (req, res) => {
     res.send("Hello.");
 });
 
 // User route
-const userRouter = require("./routes/user");
+const userRouter = require("./routes/api/user");
 app.use('/user', userRouter);
 
 // Auth route
-const authRouter = require("./routes/auth");
+const authRouter = require("./routes/api/auth");
 app.use('/auth', authRouter);
 
 // Run app (Listen on port) ----------------------------------------

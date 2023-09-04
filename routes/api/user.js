384379ed90d835f23861fe045b262e0282fada8b
Arian N. Yamchelo <ariannajafi@icloud.com>
2023-09-04 20:16:11 +1000

    Use the user model in the api user route

--- routes/api/user.js
+++ routes/api/user.js
@@ -2,24 +2,18 @@
 // User routes
 const express = require("express");
 const router = express.Router();
+const User = require("./../../models/User");
 
 // GET - get all users
 router.get('/', (req, res) => {
-    const users = [
-        {
-            _id:1,
-            firstName: "John",
-            lastName:"Smith",
-            email: "jsmith@curtin.edu.au"
-        },
-        {
-            _id:2,
-            firstName: "Arian",
-            lastName:"West",
-            email: "awest@curtin.edu.au"
-        }
-    ];
-    res.json(users);
+    // Get all users from the User model, using the find() method.
+    User.find({firstName: "A"})
+        .then((users) => {
+            res.json(users)
+        })
+        .catch((err) => {
+            console.log("No users found!", err);
+        })
 });
 
 module.exports = router;

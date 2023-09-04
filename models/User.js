351bad460806b83f5051c9a9e20e46854b4407ee
Arian N. Yamchelo <ariannajafi@icloud.com>
2023-09-04 20:15:34 +1000

    Create user model

---
+++ models/User.js
@@ -0,0 +1,39 @@
+// Dependencies
+const mongoose = require("mongoose");
+const schema = mongoose.Schema;
+require("mongoose-type-email");
+const stream = require("stream");
+
+// Create schema
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: mongoose.SchemaTypes.Email,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    bio: {
+        type: String,
+        required: false
+    },
+    accessLevel: {
+        type: Number,
+        required: true
+    }
+}, {timestamps: true});
+
+// Create mongoose model
+const userModel = mongoose.model("User", userSchema, "users");
+
+// Export as a module
+module.exports = userModel;
